6782848fdfc69b3b6ef04f036837915087d7200e
controlZ <o_dahyun@gm.gist.ac.kr>
2025-01-12 06:38:13 +0900

    📝 docs: ddfd

--- src/pages/submitContent/index.tsx
+++ src/pages/submitContent/index.tsx
@@ -11,8 +11,6 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from "react-router-dom";
-import { Account, Aptos, AptosConfig } from "@aptos-labs/ts-sdk";
-
 const SubmitContent = () => {
   const [formData, setFormData] = useState({
     title: "",
