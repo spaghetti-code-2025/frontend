980ef06ba7d1af546264060165d872d97ca53f37
controlZ <o_dahyun@gm.gist.ac.kr>
2025-01-11 21:59:30 +0900

    ✨ feat: wallet 연결 및 address 획득

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,23 @@
+import { WalletSelector } from "@/nodit/WalletSelector.tsx";
+import { Network } from "@aptos-labs/ts-sdk";
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
+
 const HomePage = () => {
-  return <></>;
+  return (
+    <AptosWalletAdapterProvider
+      autoConnect={true}
+      optInWallets={["Petra"]}
+      dappConfig={{
+        network: Network.MAINNET,
+        aptosApiKey: import.meta.env.VITE_NODIT_API_KEY,
+      }}
+      onError={(error) => {
+        console.log("error", error);
+      }}
+    >
+      <WalletSelector />
+    </AptosWalletAdapterProvider>
+  );
 };
 
 export default HomePage;
