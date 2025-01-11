f6387deeb432cc58b46efea7daab50387046a68c
crown-3 <crowntheking@gm.gist.ac.kr>
2025-01-11 17:08:46 +0900

    some initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
+import Layout from "./layout/Layout";
+import HomePage from "./pages/home";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route element={<Layout />}>
+          <Route element={<HomePage />} path="/" />
+        </Route>
+      </>,
+    ),
+  );
+
   return (
-    <div className="App">
-      <h1>hello world!</h1>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
---
+++ src/mocks/novels/novels.ts
@@ -0,0 +1,10 @@
+const NOVELS_MOCKDATA = [
+  {
+    id: 1,
+    name: "무명 식객",
+    author: "무도쟁이",
+    translation_participant_number: 266,
+  },
+];
+
+export default NOVELS_MOCKDATA;
